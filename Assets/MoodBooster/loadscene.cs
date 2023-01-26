363a636133e6ff68e40bb7b5c57b5ef68006dda7
PAP0 <patryk.podworny@gmail.com>
2023-01-26 16:23:14 +0100

    Input now works

--- Assets/MoodBooster/loadscene.cs
+++ Assets/MoodBooster/loadscene.cs
@@ -2,10 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.InputSystem;
 
 public class loadscene : MonoBehaviour
 {
     public string sceneName;
+
     public void LoadScene()
     {
         SceneManager.LoadScene(sceneName);
